9c9e16a9cce700623cef6996dae5d74b038326d5
Feng Xia <feng_xia41103@hotmail.com>
2016-10-17 14:35:22 -0400

    Update webpack.config.js to load materialize-css using materialize-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,10 @@ var config = {
         }, {
             test: /\.css$/,
             loader: "style-loader!css-loader"
-        }],
+        },{
+    test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+    loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+}],
         plugins: [
             new ExtractTextPlugin("styles.css", {
                 allChunks: false
