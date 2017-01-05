966ef2d7193c14830dd1675ca49cb02ef6a239f5
Feng Xia <feng_xia41103@hotmail.com>
2017-01-04 22:09:34 -0500

    HTML5 video as background. Need to set opacity=0.2 to make overlay text visible. Otherwise, it is working. Need to create a better video for this application.

--- src/header.jsx
+++ src/header.jsx
@@ -8,14 +8,14 @@ var Header = React.createClass({
   render () {
     return (
       <div className="myheader">
-      <video playsinline="" autoplay="" muted="" loop="" poster="/images/polina.jpg" id="bgvid">
+      <video playsInline autoPlay muted loop poster="/images/polina.jpg" id="bgvid">
              <source src="/downloads/polina.webm" type="video/webm" />
              <source src="/downloads/polina.mp4" type="video/mp4" />
       </video>
       <div className="container">
         <h1>World Snapshot</h1>
             <p>Make . Life . Better</p>
-        </div>
+      </div>
       </div>
     );
   }
