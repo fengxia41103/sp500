8a4584dd6c67f7591d442a42e4ce9c37a08bbf08
fengxia <feng_xia41103@hotmail.com>
2018-01-21 08:21:38 -0500

    Add hyperlink in footer to alpha vantage api and github source.

--- src/footer.jsx
+++ src/footer.jsx
@@ -9,14 +9,19 @@ var Footer = React.createClass({
         <div className="container">
           <h5>Data source</h5>
           <ul>
-            <li>Alpha Vantage</li>
+            <li>
+              <a href="https://www.alphavantage.co/documentation/">
+                Alpha Vantage
+              </a>
+            </li>
           </ul>
         </div>
         <div className="footer-copyright">
           <div className="container">
-            <i className="fa fa-copyright"></i>2017 PY Consulting Ltd.
+            <i className="fa fa-copyright"></i>2018 PY Consulting Ltd.
             <span className="grey-text text-lighten-4 right">
-              Made by <a href="http://fengxia.co">Feng Xia</a>
+              Made by <a href="http://fengxia.co">Feng Xia</a> @
+              <a href="https://github.com/fengxia41103/sp500">source</a>
             </span>
           </div>
         </div>
